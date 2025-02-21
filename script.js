f4b853023d3c790a480d61053c8c409404f89e8f
Emirat <lamzar031408@gmail.com>
2025-02-21 17:29:01 +0600

    fixed the button clicks

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let startTime;
 let timeout;
-let isWaiting = false;
+let isWaiting = "start";
 
 document.getElementById("button").addEventListener("click", function() {
     let playerName = document.getElementById("nameInput").value.trim();
@@ -9,27 +9,30 @@ document.getElementById("button").addEventListener("click", function() {
         return;
     }
 
-    if (!isWaiting) {
+    if (isWaiting == "start") {
         document.getElementById("result").textContent = "Ждите...";
         let button = document.getElementById("button");
         button.style.background = "#ff9f43";
+        button.innerHTML = 'Ждите...'
 
         let randomTime = Math.random() * 3000 + 1000;
-        isWaiting = true;
+        isWaiting = "stop";
 
         timeout = setTimeout(() => {
             button.style.background = "#4cd137";
+            button.innerHTML = 'Нажимайте'
             document.getElementById("result").textContent = "НАЖИМАЙТЕ!";
             startTime = Date.now();
+            isWaiting = "result";
         }, randomTime);
-    } else {
+    } else if (isWaiting == "result") {
         let reactionTime = Date.now() - startTime;
         document.getElementById("result").textContent = `Ваше время: ${reactionTime} мс`;
 
         let button = document.getElementById("button");
         button.style.background = "#ff4b5c";
         button.textContent = "Начать тест";
-        isWaiting = false;
+        isWaiting = "start";
 
         let results = JSON.parse(localStorage.getItem("reactionResults")) || [];
         results.push({ name: playerName, time: reactionTime });
